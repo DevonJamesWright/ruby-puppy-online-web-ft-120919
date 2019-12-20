8d67727c58579adf7f30be7f74ed5565aed79815
Devon James Wright <devonjamess.wright@gmail.com>
2019-12-20 16:48:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,9 +20,9 @@ attr_accessor :name
           
         end
         def self.print_all
-            print "Pluto"
-            print "Fido"
-            print "Maddy"
+            puts "Pluto"
+            puts "Fido"
+            puts "Maddy"
         
         end
 
